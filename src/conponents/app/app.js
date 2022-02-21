1cf7ff526570ae5ecff5923e0bc5d0f556f22302
Nikolay Abubakirov <undrle@yandex.ru>
2022-02-21 23:15:50 +0500

    add context...

--- src/conponents/app/app.js
+++ src/conponents/app/app.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './app.css';
-import { withBookstoreService } from '../hoc';
+import { Route, Routes } from 'react-router-dom';
+import { HomePage, CartPage } from '../pages';
 
-const App = ({ bookstoreService }) => {
-  console.log(bookstoreService.getBooks());
+const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Routes>
+      <Route path='/'
+        element={<HomePage />}
+        exact />
+
+      <Route path='/cart'
+        element={<CartPage />}
+        exact />
+    </Routes>
   );
 };
 
-export default withBookstoreService()(App);
+export default App;
